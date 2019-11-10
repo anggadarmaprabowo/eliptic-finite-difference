7f8aa4a8e7bf7f50d413dbcedc8ed3b0854934cb
anggadarmaprabowo <57395082+anggadarmaprabowo@users.noreply.github.com>
2019-11-10 08:48:49 +0700

    create first file

---
+++ eliptic_finite_difference_2_order.py
@@ -0,0 +1,97 @@
+import numpy as np
+
+main_matrix = []
+len_row_matrix = 5
+len_column_matrix = 5
+column = 0
+batas_atas = 100
+batas_bawah = 10
+batas_kanan = 50
+batas_kiri = 75
+
+# membuat dimensi node matriks beda hingga
+
+for j in range(len_row_matrix):
+    row_matrix = []
+    for i in range(len_column_matrix):
+        row_matrix.append(0)
+    print(row_matrix)
+    main_matrix.append(row_matrix)
+    column+=1
+
+print( )
+
+# mendefinisikan kondisi batas pada matriks beda hingga
+
+main_matrix[0][0] = (batas_atas + batas_kiri)/2
+main_matrix[len_row_matrix-1][0] = (batas_kiri + batas_bawah)/2
+main_matrix[0][len_row_matrix-1] = (batas_atas + batas_kanan)/2
+main_matrix[len_row_matrix-1][len_column_matrix-1] = (batas_kanan+batas_bawah)/2
+
+for i in range(1,len_column_matrix-1):
+    main_matrix[0][i] = batas_atas
+
+for i in range(1,len_column_matrix-1):
+    main_matrix[len_row_matrix-1][i] = batas_bawah
+
+for i in range(1,len_row_matrix-1):
+    main_matrix[i][len_column_matrix-1] = batas_kanan
+
+for i in range(1,len_row_matrix-1):
+    main_matrix[i][0] = batas_kiri
+
+for i in range(len_row_matrix):
+    print(main_matrix[i])
+
+print( )
+# operasi perhitungan tiap node
+
+count_matrix = []
+len_matrix = (len_row_matrix-2) * (len_column_matrix-2)
+print(len_matrix)
+for i in range(len_matrix):
+    row_matrix_count = []
+    for j in range(len_matrix):
+        row_matrix_count.append(0)
+    #print(row_matrix_count)
+    count_matrix.append(row_matrix_count)
+    column+=1
+
+print(" ")
+for i in range(len_matrix):
+    for j in range(len_matrix):
+        if j == i:
+            count_matrix[i][j] = 4
+
+#for i in range(len_matrix):
+    #print(count_matrix[i])
+
+target_matrix = []
+
+
+for m in range(1,len_column_matrix-1):
+    row_target_matrix = []
+
+    for n in range(1,len_row_matrix-1):
+        if main_matrix[m-1][n] != 0:
+            jumlah_target_1 = main_matrix[m-1][n]
+        else:
+            jumlah_target_1 = 0
+            count_matrix[(((len_column_matrix-2)*(m-1)+n))-1][(((len_column_matrix-2)*(m-2))+n)-1] = -1
+
+for m in range(1, len_column_matrix - 1):
+    row_target_matrix = []
+
+    for n in range(1, len_row_matrix - 1):
+        if main_matrix[m + 1][n] != 0:
+            jumlah_target_2 = main_matrix[m + 1][n]
+        else:
+            jumlah_target_2 = 0
+            count_matrix[((((len_column_matrix - 2) * (m-1))) + n)-1][(((len_column_matrix - 2) * (m)) + n) - 1] = -1
+            print(n,m)
+
+    #umlah_target = jumlah_target_1 + jumlah_target_2 + jumlah_target_3 + jumlah_target_4
+    #row_target_matrix.append(jumlah_target)
+    print(" ")
+    for i in range(len_matrix):
+        print(count_matrix[i])
